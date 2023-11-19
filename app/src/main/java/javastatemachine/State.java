bc1889772401b2fe9623b284cffa046b792a968b
Jack Lingle <captainjack0818@gmail.com>
2023-11-19 14:14:22 -0500

    add Transitions class

--- app/src/main/java/javastatemachine/State.java
+++ app/src/main/java/javastatemachine/State.java
@@ -1,5 +1,17 @@
 package javastatemachine;
 
+import javastatemachine.Transitions;
+
 public class State {
-  
+  String name;
+  Transitions edges;
+
+  public State (String n) {
+    name = n;
+    edges = Transitions(this);
+  }
+  public State (String n, String[] edges) {
+    name = n;
+    this.edges = Transitions(this, edges);
+  }
 }
--- app/src/main/java/javastatemachine/Transitions.java
+++ app/src/main/java/javastatemachine/Transitions.java
@@ -5,12 +5,22 @@ import javastatemachine.State;
 public class Transitions {
   State baseState;
   State[] edges;
+  public Transitions(State base) {
+    this.baseState = base;
+    this.edges = new State[4];
+  }
   public Transitions (String baseState, String[] edges) {
     this.baseState = new State(baseState);
     this.edges = new State[edges.length()];
     for (int i = 0; i < edges.length(); i++) {
       this.edges[i] = new State(edges[i]);
     }
-
+  }
+  public Transitions (State base, String[] edges) {
+    this.baseState = base;
+    this.edges = new State[edges.length()];
+    for (int i = 0; i < edges.length(); i++) {
+      this.edges[i] = new State(edges[i]);
+    }
   }
 }
