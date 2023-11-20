b6785491865016d4f694eb74cc340a225098940c
Jack Lingle <captainjack0818@gmail.com>
2023-11-20 07:23:25 -0500

    fix errors regarding method calls

--- app/src/main/java/javastatemachine/State.java
+++ app/src/main/java/javastatemachine/State.java
@@ -1,17 +1,15 @@
 package javastatemachine;
 
-import javastatemachine.Transitions;
-
 public class State {
   String name;
   Transitions edges;
 
   public State (String n) {
     name = n;
-    edges = Transitions(this);
+    edges = new Transitions(this);
   }
-  public State (String n, String[] edges) {
-    name = n;
-    this.edges = Transitions(this, edges);
-  }
+//  public State (String n, String[] edges) {
+//    name = n;
+//    this.edges = new Transitions(this, edges);
+//  }
 }
